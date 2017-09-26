f3616256d73bb08adc1722ba6f5a6ea3440e46b2
Your Name <you@example.com>
2017-09-26 12:12:56 +0200

    pravki

--- app/mailers/project_mailer.rb
+++ app/mailers/project_mailer.rb
@@ -1,6 +1,6 @@
 class ProjectMailer < ApplicationMailer
   def send_project_mail(project)
     @project = project
-    mail(to: "danielpogosyan18@gmail.com", subject: "Заявка на обсуждение")
+    mail(to: "info@biglions.org", subject: "Заявка на обсуждение")
   end
 end
