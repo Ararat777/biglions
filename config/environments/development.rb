f96ec4c3b60578e3aab476e249dd97424675cafd
Your Name <you@example.com>
2017-09-27 10:17:40 +0200

    text

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,9 +27,18 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
-  config.action_mailer.perform_caching = false
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+   :address              => "smtp.beget.com",
+   :port                 => '465',
+   :domain               => "beget.com",
+   :user_name            => "info@biglions.org",
+   :password             => "infobiglions13",
+   :authentication       => :login,
+   :enable_starttls_auto => true,
+   :openssl_verify_mode  => 'none'
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
