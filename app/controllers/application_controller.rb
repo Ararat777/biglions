9796f1fac81511bfb026980e27b878001d3eadaf
Your Name <you@example.com>
2017-09-28 14:12:34 +0200

    ipad

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def mobile_device?
-    request.user_agent =~ /Mobile|webOS/
+    (request.user_agent =~ /Mobile|webOS/) && (request.user_agent !~ /iPad/)
   end
   helper_method :mobile_device?
 end
