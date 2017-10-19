b7e939824ea7ef177fbac95f71e55079a081c4de
Your Name <you@example.com>
2017-10-19 19:35:58 +0200

    reviews

--- app/admin/question.rb
+++ app/admin/question.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register Question do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-  permit_params :body,:answer
+  permit_params :body,:answer,:category_id
 #
 # or
 #
