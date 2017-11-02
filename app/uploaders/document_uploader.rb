f3929a109fec92a55b07644fad89d4332664e58c
Your Name <you@example.com>
2017-11-02 13:21:00 +0100

    pravki

--- app/uploaders/document_uploader.rb
+++ app/uploaders/document_uploader.rb
@@ -14,7 +14,7 @@ class DocumentUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
   def extension_whitelist
-    %w(doc docx pdf)
+    %w(doc docx pdf txt)
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
